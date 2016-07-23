a898295dcb38972f5821ee2aa06a54c0dc3d35d1
mullfuchs <burke.leo@gmail.com>
2016-07-22 17:00:05 -0700

    pushing nightly work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 console.log('Hello frontend');
+
+var board = document.getElementById('board');
+
+var playerOnePlaying = true;
+
+//board.addEventListener('click', changeBGColor);
+  
+var squares = document.querySelectorAll("div.square");
+
+for(var i = 0; i < squares.length; i++){
+	squares[i].addEventListener("click", changeBGColor)
+	squares[i].hasBeenClicked = false;
+}
+  
+function changeBGColor(){
+	if(!this.hasBeenClicked){
+		if(playerOnePlaying){
+	  		this.style.backgroundColor = "red";	
+		}
+		else{
+			this.style.backgroundColor = "green";
+		}
+		this.hasBeenClicked = true;
+		playerOnePlaying = !playerOnePlaying;
+	}
+}
